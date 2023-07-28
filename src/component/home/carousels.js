98679b0ac0d3a6a74b0da7066bf7073d74b2a53c
정남용 <jny5360@gmail.com>
2023-07-28 20:18:28 +0900

    메인페이지 사진 삽입

--- src/component/home/carousels.js
+++ src/component/home/carousels.js
@@ -1,4 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
+import solution_img from './solution_img.jpg'
+import insight_img from './insight.jpg'
+import revolution_img from './revolution_img.webp'
+import intelligenc_img from './Intelligence_img.jpg'
 
 
 function UncontrolledExample() {
@@ -6,12 +10,11 @@ function UncontrolledExample() {
     <Carousel>
     <Carousel.Item>
         <div className='slidercontents'>
+            <img src={solution_img} className='solution_img'></img>
             <div className='wrapText'>
-                <h1>Organic fresh fruits for your health</h1>
+                <h1>Solution</h1>
                 <div className="d-none d-md-block">
-                    <p>Interdum et malesuada fames ac ante ipsum primis in 
-                    faucibus. Mauris eleifend sagittis mollis. 
-                    Nulla finibus arcu eu tortor gravida aliquet</p>
+                    <h3>대충 업체에게 정확한 솔루션을 제공해준다는 말</h3>
                 </div>
                 
             </div>
@@ -19,12 +22,12 @@ function UncontrolledExample() {
     </Carousel.Item>
     <Carousel.Item>
         <div className='slidercontents2'>
+            <img src={insight_img} className='insight_img'></img>
+
             <div className='wrapText'>
-                <h1>Organic fresh fruits for your health</h1>
+                <h1>Insight</h1>
                 <div className="d-none d-md-block">
-                    <p>Interdum et malesuada fames ac ante ipsum primis in 
-                    faucibus. Mauris eleifend sagittis mollis. 
-                    Nulla finibus arcu eu tortor gravida aliquet</p>
+                    <h3>대충 인사이트 어쩌고 저쩌고</h3>
                 </div>
                 
             </div>
@@ -32,12 +35,23 @@ function UncontrolledExample() {
         </Carousel.Item>
         <Carousel.Item>
         <div className='slidercontents3'>
+            <img src={revolution_img} className='revolution_img'></img>
             <div className='wrapText'>
-                <h1>Organic fresh fruits for your health</h1>
+                <h1>Revolution</h1>
                 <div className="d-none d-md-block">
-                    <p>Interdum et malesuada fames ac ante ipsum primis in 
-                    faucibus. Mauris eleifend sagittis mollis. 
-                    Nulla finibus arcu eu tortor gravida aliquet</p>
+                    <h3>대충 우리업체는 레볼루션 어쩌구</h3>
+                </div>
+                
+            </div>
+        </div>
+    </Carousel.Item>
+    <Carousel.Item>
+        <div className='slidercontents4'>
+            <img src={intelligenc_img} className='intelligenc_img'></img>
+            <div className='wrapText'>
+                <h1>intelligence</h1>
+                <div className="d-none d-md-block">
+                    <h3>대충 우리업체 능지 최고 ^.^</h3>
                 </div>
                 
             </div>
