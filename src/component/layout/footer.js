fbd67dbc6e29136fa6bc276bd29a53d25172834f
limsehui <sehui9072@gmail.com>
2023-07-31 17:59:09 +0900

    footer
    푸터수정

--- src/component/layout/footer.js
+++ src/component/layout/footer.js
@@ -4,8 +4,11 @@ const Footer = () =>{
     return (
         <footer className="footer">
             
-                <small>Copyright &copy;Sehui</small>
-            
+                <small className='footer_copy'>Copyright &copy;Sehui</small>
+
+                <small>광주광역시 동구 제봉로 92 대성학원 3층</small>
+                <small>sehui9072@gmali.com</small>
+
         </footer>
     )
 }
