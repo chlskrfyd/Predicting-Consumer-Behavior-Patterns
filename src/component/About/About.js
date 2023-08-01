5caa64328cf889ad366c5505f4cc08798c7e7f25
정남용 <jny5360@gmail.com>
2023-08-01 13:52:53 +0900

    About 수정완료 0801 / 0152

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -10,16 +10,16 @@ const About = () => {
   return (
     <body className="about">
       <div className='main-container'>
-        <div class="temp-box-title">
+        <div className="temp-box-title">
           회사소개
         </div>
-        <div class="temp-box">
-          <div class="temp-box-2">
+        <div className="temp-box">
+          <div className="temp-box-2">
             <img src={building_img} className='building_img' alt='bulding_img'></img>
           </div>
         </div>
 
-        <div class="temp-box-title">팀원소개</div>
+        <div className="temp-box-title">팀원소개</div>
 
         <div className="temp-box-4">
           <div className="image-group">
@@ -60,13 +60,13 @@ const About = () => {
             </div>                        
           </div>
         </div>
-        
-        <div class="temp-box-title">Contact us</div>
+
+        <div className="temp-box-title">Contact us</div>
 
 
-        <div class="temp-box">6</div>
+        <div className="temp-box">6</div>
+
       </div>
-
     </body>
   )
 }
