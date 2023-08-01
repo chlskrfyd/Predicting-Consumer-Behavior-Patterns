acce841850be9d1b33a985ca45ae587db98bddc9
정남용 <jny5360@gmail.com>
2023-08-01 12:01:39 +0900

    팀원소개 완료

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -6,35 +6,69 @@ import 세희 from "./세희.jpg"
 import 보은 from "./보은.jpg"
 import 지혜 from "./지혜.jpg"
 
-const About = () =>{
-    return (
-        <body className="about">
-          <div className='main-container'>
-            <div class="temp-box-title">
-              회사소개
-            </div>
-            <div class="temp-box">
-              <div class ="temp-box-2">
-                <img src={building_img} className='building_img' alt='bulding_img'></img>
-              </div>
-            </div>
-
-            <div class="temp-box-title">팀원소개</div>
-
-            <div class="temp-box-4">4</div>
-              <div className='member_pic'></div>
-                <img src={보은} className='보은' alt='보은'></img>
-                <img src={지혜} className='지혜' alt='지혜'></img>
-                <img src={남용} className='남용' alt='남용'></img>
-                <img src={낙룡} className='낙룡' alt='낙룡'></img>
-                <img src={세희} className='세희' alt='세희'></img>
-            <div class="temp-box-title">Contact us</div>
-
-            <div class="temp-box">6</div>
+const About = () => {
+  return (
+    <body className="about">
+      <div className='main-container'>
+        <div class="temp-box-title">
+          회사소개
+        </div>
+        <div class="temp-box">
+          <div class="temp-box-2">
+            <img src={building_img} className='building_img' alt='bulding_img'></img>
           </div>
-          
-        </body>
-    )
+        </div>
+
+        <div class="temp-box-title">팀원소개</div>
+
+        <div className="temp-box-4">
+          <div className="image-group">
+            <div className="image-container">
+              <img src={보은} alt="보은" className="mem-pic" />
+                <div className="image-caption">
+                  <p>박보은</p>
+                  <p1>프로젝트 팀장</p1>
+                </div>
+            </div>
+            <div className="image-container">
+              <img src={지혜} alt="지혜" className="mem-pic" />
+                <div className="image-caption">
+                  <p>김지혜</p>
+                  <p1>데이터수집 팀장</p1>
+                </div>
+            </div>
+            <div className="image-container">
+              <img src={낙룡} alt="남용" className="mem-pic" />
+                <div className="image-caption">
+                  <p>최낙룡</p>
+                  <p1>데이터분석 팀장</p1>
+                </div>
+            </div>
+            <div className="image-container">
+              <img src={세희} alt="세희" className="mem-pic" />
+                <div className="image-caption">
+                  <p>임세희</p>
+                  <p1>프론트엔드 팀장</p1>
+                </div>
+            </div>
+            <div className="image-container">
+              <img src={남용} alt="남용" className="mem-pic" />
+                <div className="image-caption">
+                  <p>정남용</p>
+                  <p1>프론트엔드 팀원</p1>
+                </div>
+            </div>                        
+          </div>
+        </div>
+        
+        <div class="temp-box-title">Contact us</div>
+
+
+        <div class="temp-box">6</div>
+      </div>
+
+    </body>
+  )
 }
 
 export default About;
