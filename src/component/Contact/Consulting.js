f273a777861b6f3daf284f38d90aa182ba6699fb
limsehui <sehui9072@gmail.com>
2023-08-03 10:08:48 +0900

    컨설팅 페이지 수정
    컨설팅 페이지 아마존 로고 삽입

--- src/component/Contact/Consulting.js
+++ src/component/Contact/Consulting.js
@@ -1,28 +1,28 @@
 import './Consulting.css'
+import amazon from "./amazon.png"
 
 const Contact = () =>{
     return (
-        <body className="contact">
-            
-          <div className='main-container'>
-            <div class="temp-box-title">
-              기업컨설팅
-            </div>
-            <div class="temp-box-title">
-              소비자 행동분석
-            </div>
-            <div class="temp-box">6</div>
-            <div class="temp-box-title">
-              소비자 감정분석
-            </div>
-            <div class="temp-box">6</div>
-            <div class="temp-box-title">
-              이탈고객예측
-            </div>
-            <div class="temp-box">6</div>
-          </div>
-            
-        </body>
+<body className="consulting">
+      <div className='about_main'>
+        <h1 className="consulting_main_title">기업 컨설팅</h1>
+        
+        <div className="consulting_title">소비자 행동분석
+        <img src={amazon} className='amazon' alt='amazon'></img>
+        </div>
+        <div className="consulting_box_2"></div>
+
+        <div className="consulting_title">감정분석
+        <img src={amazon} className='amazon_2' alt='amazon'></img>
+        </div>
+        <div className="consulting_box_2"></div>
+
+        <div className="consulting_title">이탈고객 예측
+        <img src={amazon} className='amazon' alt='amazon'></img>
+        </div>
+        <div className="consulting_box_2"></div>   
+      </div>
+    </body>
     )
 }
 
