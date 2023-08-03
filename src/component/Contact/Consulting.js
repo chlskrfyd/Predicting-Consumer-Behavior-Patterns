e696217f4047cf3428b4929ab4fe9fc481c8d219
limsehui <sehui9072@gmail.com>
2023-08-03 17:04:20 +0900

    데이터 들어갈자리
    consulting.js
    <div className="consulting_box_2">
    >> 이 안에 데이터시각화 들어가면 됨
    </div>

--- src/component/Contact/Consulting.js
+++ src/component/Contact/Consulting.js
@@ -10,7 +10,9 @@ const Contact = () =>{
         <div className="consulting_title">소비자 행동분석
         <img src={amazon} className='amazon' alt='amazon'></img>
         </div>
-        <div className="consulting_box_2"></div>
+        <div className="consulting_box_2">
+          데이터 분석 내용 들어갈 자리
+        </div>
 
         <div className="consulting_title">감정분석
         <img src={amazon} className='amazon_2' alt='amazon'></img>
