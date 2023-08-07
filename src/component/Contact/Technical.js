d3e0489abf078fc5a3f4287974160a5192ad8865
정남용 <jny5360@gmail.com>
2023-08-07 19:12:41 +0900

    스타벅스 분석 데이터 입력

--- src/component/Contact/Technical.js
+++ src/component/Contact/Technical.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Technical.css";
 import starbucks from "./startbucks.png";
+import coffee_consumption from "./starbucks/coffee_consumption.png";
 import amazon from "./amazon.png";
 import createPlotlyComponent from "react-plotly.js/factory";
 import graphData from "./graphData.json";
@@ -17,7 +18,25 @@ const Tachnical = () => {
         </div>
 
         <div className="technical_box_2">
-          <p>개요 : 증가하는 커피 소비량에 맞춰 스타벅스 지점 예측 서비스</p>
+          <br />
+          <p className="starbucks_text_box">
+            분석 주제 : 스타벅스 입점 국가 예측
+          </p>
+          <p className="starbucks_text_box">
+            개요 : 커피 소비량은 전 세계적으로 증가하고 있다. <br></br>
+            이러한 추세를 반영하여 국가 별 커피소비량과 국가 별 스타벅스 입점
+            지점 데이터를 활용하고 입점국가를 예측하는 서비스를 제공한다.
+          </p>
+          <br></br>
+          <figure>
+            <img src={coffee_consumption} alt="coffee_consumption" />
+            <figcaption>
+              2021년 전 세계 커피 소비량이 커피 생산량을 넘어섰다.
+              <br />
+              이미지 출처 : ICO(https://www.ico.org)
+            </figcaption>
+          </figure>
+
           <Plot
             data={graphData.data}
             layout={graphData.layout}
