a5294e3fe23294b9e80ba9472e86d613b264d97c
정남용 <jny5360@gmail.com>
2023-08-08 15:28:34 +0900

    고객이탈예측 이미지 업로드

--- src/component/Contact/Consulting.js
+++ src/component/Contact/Consulting.js
@@ -1,31 +1,45 @@
-import './Consulting.css'
-import amazon from "./amazon.png"
+import "./Consulting.css";
+import amazon from "./amazon/amazon.png";
+import 고객이탈예측1 from "./amazon/amazon_고객이탈예측/고객이탈예측1.png";
+import 고객이탈예측2 from "./amazon/amazon_고객이탈예측/고객이탈예측2.png";
+import 고객이탈예측3_1 from "./amazon/amazon_고객이탈예측/고객이탈예측3-1.png";
+import 고객이탈예측3_2 from "./amazon/amazon_고객이탈예측/고객이탈예측3-2.png";
+import 고객이탈예측4 from "./amazon/amazon_고객이탈예측/고객이탈예측4.png";
 
-const Consulting = () =>{
-    return (
-<body className="consulting">
-      <div className='about_main'>
-        <h1 className="consulting_main_title">기업 컨설팅</h1>
-        
-        <div className="consulting_title">고객 이탈 예측
-        <img src={amazon} className='amazon_1' alt='amazon'></img>
+const Consulting = () => {
+  return (
+    <body className="consulting">
+      <div className="about_main">
+        <h1 className="consulting_main_title">Cousultings</h1>
+        <div className="consulting_title">
+          고객 이탈 예측
+          <img src={amazon} className="amazon_1" alt="amazon"></img>
         </div>
         <div className="consulting_box_2">
-          데이터 분석 내용 들어갈 자리
+          <img src={고객이탈예측1} alt="고객이탈예측1"></img>
+          <img src={고객이탈예측2} alt="고객이탈예측2"></img>
+          <br />
+          <img src={고객이탈예측3_1} alt="고객이탈예측3-1"></img>
+          <br />
+          <img src={고객이탈예측3_2} alt="고객이탈예측3-2"></img>
+          <br />
+          <img src={고객이탈예측4} alt="고객이탈예측4"></img>
         </div>
 
-        <div className="consulting_title">구매 카테고리 예측
-        <img src={amazon} className='amazon_2' alt='amazon'></img>
+        <div className="consulting_title">
+          구매 카테고리 예측
+          <img src={amazon} className="amazon_2" alt="amazon"></img>
         </div>
         <div className="consulting_box_2"></div>
 
-        <div className="consulting_title"> 고객 세분화
-        <img src={amazon} className='amazon_3' alt='amazon'></img>
+        <div className="consulting_title">
+          고객 세분화
+          <img src={amazon} className="amazon_3" alt="amazon"></img>
         </div>
-        <div className="consulting_box_2"></div>   
+        <div className="consulting_box_2"></div>
       </div>
     </body>
-    )
-}
+  );
+};
 
 export default Consulting;
