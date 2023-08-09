3608ea4a79e1ef5980eab9d56ea7f22344473907
정남용 <jny5360@gmail.com>
2023-08-09 09:35:31 +0900

    자잘한 것 수정

--- src/component/Contact/Consulting.js
+++ src/component/Contact/Consulting.js
@@ -1,20 +1,26 @@
 import "./Consulting.css";
 import amazon from "./amazon/amazon.png";
+import createPlotlyComponent from "react-plotly.js/factory";
+import Plot from "react-plotly.js";
+
+// 고객 이탈 예측
 import 고객이탈예측1 from "./amazon/amazon_고객이탈예측/고객이탈예측1.png";
 import 고객이탈예측2 from "./amazon/amazon_고객이탈예측/고객이탈예측2.png";
 import 고객이탈예측3_1 from "./amazon/amazon_고객이탈예측/고객이탈예측3-1.png";
 import 고객이탈예측3_2 from "./amazon/amazon_고객이탈예측/고객이탈예측3-2.png";
 import 고객이탈예측4 from "./amazon/amazon_고객이탈예측/고객이탈예측4.png";
+
+// 구매 카테고리 예측
 import buy_prediction1 from "./amazon/amazon_구매카테고리예측/buy_predict1.json";
 import buy_prediction2 from "./amazon/amazon_구매카테고리예측/buy_predict2.json";
 import just1 from "./amazon/amazon_구매카테고리예측/just1.json";
 import just2 from "./amazon/amazon_구매카테고리예측/just2.json";
 import 구매예측4 from "./amazon/amazon_구매카테고리예측/구매예측4.png";
+
+// 고객 세분화
 import segmentation1 from "./amazon/amazon_고객세분화/customer_segmentation1.json";
 import segmentation2 from "./amazon/amazon_고객세분화/customer_segmentation2.json";
 import segmentation3 from "./amazon/amazon_고객세분화/customer_segmentation3.json";
-import createPlotlyComponent from "react-plotly.js/factory";
-import Plot from "react-plotly.js";
 
 const Consulting = () => {
   return (
