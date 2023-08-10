90b03fadba96d32f9acb83ed77771a2866846c1f
정남용 <jny5360@gmail.com>
2023-08-10 12:06:10 +0900

    고객세분화 글씨 삽입

--- src/component/Contact/Customer.js
+++ src/component/Contact/Customer.js
@@ -16,24 +16,34 @@ const customer = () => {
       <div className="about_main">
         <h1 className="customer_main_title">고객이탈예측</h1>
         <div className="customer_main_txt">
-        고객의 구매 패턴, 브라우징 빈도, 리뷰 남김 여부 등의 정보를 활용하여 고객 이탈을 예측하는 모델 
-        <br></br>이를 통해 이탈할 가능성이 높은 고객에게 특별한 제안을 보내거나 개인화된 경험을 제공하여 이탈을 방지할 수 있습니다.
+          고객의 구매 패턴, 브라우징 빈도, 리뷰 남김 여부 등의 정보를 활용하여
+          고객 이탈을 예측하는 모델
+          <br></br>이를 통해 이탈할 가능성이 높은 고객에게 특별한 제안을
+          보내거나 개인화된 경험을 제공하여 이탈을 방지할 수 있습니다.
         </div>
 
-        <div className="customer_title">고객 이탈률을 선정하는데 있어 어떤 특성이 가장 중요한가</div>
-          <div className="customer_img">
-            <img src={고객이탈예측1} alt="고객이탈예측1"></img>
-          </div>
-          <div className="customer_txt"> ➡ 검색 횟수,소비자 리뷰 중요도, 쇼핑 만족도 등이 고객 이탈률을 계산하는데 있어서 중요한 경향이 있는 것을 알 수 있었다.</div>
-        
-
         <div className="customer_title">
-          검색 횟수와 이탈률의 상관관계</div>
-          <div className="customer_img">
-            <img src={고객이탈예측2} alt="고객이탈예측2"></img>
-          </div>
-          <div className="customer_txt"> ➡ 그래프에서 가장 중요한 특성을확인해보기로하였다. 검색 횟수가 낮을수록 고객 이탈률이 높다.</div>
-     </div>
+          고객 이탈률을 선정하는데 있어 어떤 특성이 가장 중요한가
+        </div>
+        <div className="customer_img">
+          <img src={고객이탈예측1} alt="고객이탈예측1"></img>
+        </div>
+        <div className="customer_txt">
+          {" "}
+          ➡ 검색 횟수,소비자 리뷰 중요도, 쇼핑 만족도 등이 고객 이탈률을
+          계산하는데 있어서 중요한 경향이 있는 것을 알 수 있었다.
+        </div>
+
+        <div className="customer_title">검색 횟수와 이탈률의 상관관계</div>
+        <div className="customer_img">
+          <img src={고객이탈예측2} alt="고객이탈예측2"></img>
+        </div>
+        <div className="customer_txt">
+          {" "}
+          ➡ 그래프에서 가장 중요한 특성을확인해보기로하였다. 검색 횟수가
+          낮을수록 고객 이탈률이 높다.
+        </div>
+      </div>
     </body>
   );
 };
--- src/component/Contact/Segmentation.js
+++ src/component/Contact/Segmentation.js
@@ -12,31 +12,75 @@ const Segmentation = () => {
   return (
     <body className="segmentation">
       <div className="about_main">
-        <h1 className="segmentation_main_title">segmentation</h1>
-
-        <div className="segmentation_title">segmentation</div>
-        <div className="segmentation_box">asdfa</div>
-
-        <div className="segmentation_title">
-          고객 세분화
-          <img src={amazon} className="amazon_3" alt="amazon"></img>
+        <h1 className="segmentation_main_title">고객 세분화</h1>
+        <div className="segmentation_main_txt">
+          고객 세분화는 고객을 공통된 특성을 공유하는 그룹으로 분류하는
+          것입니다.
+          <br /> <br />
+          이 데이터셋을 활용하면, 고객의 구매 패턴, 제품 검색 방법, 리뷰 남김
+          여부, '나중에 저장하기' 기능 사용 빈도 등을 바탕으로 고객을 여러
+          그룹으로 분류할 수 있습니다.
+          <br />
+          이런 세분화는 마케팅 전략을 개발하거나 개인화된 경험을 제공하는 데
+          유용합니다.
         </div>
-        <div className="segmentation_box_2"></div>
-        <Plot // customer_segmentation1
-          data={segmentation1.data}
-          layout={segmentation1.layout}
-          config={{ responsive: true }}
-        />
+        <div className="segmentation_title">
+          머신러닝 모델을 사용하여 고객을 4개의 그룹으로 세분화했다.
+        </div>
+        <br /> <br />
         <Plot // customer_segmentation2
           data={segmentation2.data}
           layout={segmentation2.layout}
           config={{ responsive: true }}
         />
+        <div className="segmentation_box">
+          ➡고객들을 세분화 한 특성들을 2차원으로 차원 축소하여 보기 편하게
+          만들었다.
+          <br />
+          ➡위 그래프를 볼 때 고객들이 잘 세분화되어 나누어져 있는 것을 확인할 수
+          있었다.
+        </div>
+        <div className="segmentation_title">그룹별 특징</div>
+        <br />
+        <br />
+        <Plot // customer_segmentation1
+          data={segmentation1.data}
+          layout={segmentation1.layout}
+          config={{ responsive: true }}
+        />
+        <div className="segmentation_box">
+          <div>
+            ➡그룹 0: 개인화된 추천 빈도가 높고, 검색 횟수는 낮은 편이다.
+          </div>
+          <div>
+            ➡그룹 1:나이가 가장 어리고 고객 리뷰를 중요하게 생각하지않는다.
+          </div>
+          <div>
+            ➡그룹 2:검색 횟수가 가장 높으며, 고객 리뷰를 중요하게 생각하며
+            개인화된 추천 빈도와 장바구니 완성 빈도는 낮다.
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;제품을 검색할때 주로 다른
+            방법(other)으로 검색한다.
+          </div>
+          <div>
+            ➡그룹 3: 나이가 가장 높으며, 개인화된 추천 빈도와 검색 횟수가
+            낮고,장바구니 사용빈도가 낮음
+          </div>
+        </div>
+        <div className="segmentation_title">
+          제품 선택 방법에 따른 그룹의 특징
+        </div>
+        <br />
+        <br />
         <Plot // customer_segmentation3
           data={segmentation3.data}
           layout={segmentation3.layout}
           config={{ responsive: true }}
         />
+        <div className="segmentation_box">
+          ➡2번 그룹이 다른 고객들에 비해 제품을 고를 때 키워드,태그와 다른
+          방법을 사용한 경우가 많았다.
+        </div>
       </div>
     </body>
   );
